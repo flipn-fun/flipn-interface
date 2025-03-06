02f4bfe73416a4153dd35641721986cacdfc628d
emily <emily@ref.finance>
2025-03-06 16:23:14 +0800

    add limitation to holders.

--- app/sections/home/laptop/token/index.tsx
+++ app/sections/home/laptop/token/index.tsx
@@ -34,7 +34,7 @@ export default function Token({
   const { innerHeight, innerWidth } = useUserAgent();
   const descContentRef = useRef<any>();
 
-  const { total: totalHolders } = useHolders(token);
+  const { total: totalHolders } = useHolders(token, dataAvailable);
 
   return (
     <div
