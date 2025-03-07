091798bfa9ead55b8de6eff921e625e5530b5e84
jimmygu <jimmyguu2024@gmail.com>
2025-03-08 00:47:54 +0800

    feat: Add forwardRef to CouponCard and manage state via parent
    Updated CouponCard to use React.forwardRef, enabling the parent component to control its internal state via useImperativeHandle. Adjusted InviterConnect to interact with the new ref for better state synchronization on visibility changes.

--- app/sections/invite-code/components/inviter-connect/index.tsx
+++ app/sections/invite-code/components/inviter-connect/index.tsx
@@ -1,6 +1,6 @@
 import styles from "./index.module.css";
 import CouponCard from '@/app/sections/invite-code/components/coupon-card';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { formatLongText } from '@/app/utils/common';
 import { useWalletModal } from '@/app/libs/solana/wallet-adapter/modal';
 import { removeAuth } from '@/app/utils';
@@ -10,13 +10,22 @@ const InviterConnect = (props: any) => {
 
   const { visible, setVisible } = useWalletModal();
 
+  const couponCardRef = useRef<any>();
+
   const handleConnect = () => {
     removeAuth();
     setVisible(!visible);
   };
 
+  useEffect(() => {
+    if (!visible) {
+      couponCardRef.current?.setIsStarted?.(false);
+    }
+  }, [visible]);
+
   return (
     <CouponCard
+      ref={couponCardRef}
       className={className}
       disabled={false}
       loading={false}
