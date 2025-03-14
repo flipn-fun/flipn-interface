5822d1035efff224fabfdfce6093eec4eca9eef4
ben <benjm240@gmail.com>
2025-03-14 11:16:55 +0800

    change token url to json

--- app/sections/create/components/create/content.tsx
+++ app/sections/create/components/create/content.tsx
@@ -5,6 +5,7 @@ import styles from "./trande.module.css";
 import MainBtn from "@/app/components/mainBtn";
 import { useTokenTrade } from "@/app/hooks/useTokenTrade";
 import {
+  generateRandomString,
   getFullNum,
   getPointByVolume,
   getTransaction,
@@ -15,6 +16,7 @@ import type { Project } from "@/app/type";
 import { fail } from "@/app/utils/toast";
 import { useUserAgent } from "@/app/context/user-agent";
 import useBalance from "@/app/hooks/useBalance";
+import { postUpload } from "@/app/utils";
 import { numberFormatter } from "@/app/utils/common";
 import CreateSuccessModal from "../createSuccessModal";
 import { useConfig } from "@/app/store/useConfig";
@@ -196,10 +198,16 @@ export default function Create({
 
         await onBeforeCreate();
 
+        const filePath = await uploadTokenMeta(data)
+
+        if (!filePath) {
+          throw "Upload token meta error";
+        }
+
         const hash = await createToken({
           name: tokenName,
           symbol: tokenSymbol,
-          uri: tokenUri,
+          uri: filePath,
           launching: launchChecked,
           amount:
             ignorePrepaid !== 0 && totalRef.current.inputVal
@@ -393,4 +401,24 @@ export default function Create({
       />
     </>
   );
+}
+
+
+async function uploadTokenMeta(token: Project) {
+  const metaData = {
+    "name": token.tokenName,
+    "symbol": token.ticker,
+    "description": token.about,
+    "image": token.tokenImg,
+    "twitter": token.x,
+    "website": token.website,
+  }
+
+  const fileName = generateRandomString(10)
+
+  const blob = new Blob([JSON.stringify(metaData)], { type: 'application/json' });
+  const file = new File([blob], token.tokenName + token.ticker + '.json', { type: 'application/json' });
+  const filePath = await postUpload(file, fileName + '.json', 'application/json');
+
+  return filePath;
 }
