be63a898a80f0563c9925f8edf915f35137a46ec
ben <benjm240@gmail.com>
2025-03-19 10:00:17 +0800

    add report data

--- app/components/trade/buySellPump/index.tsx
+++ app/components/trade/buySellPump/index.tsx
@@ -23,6 +23,7 @@ import { useConnection } from "@solana/wallet-adapter-react";
 import { numberFormatter } from "@/app/utils/common";
 import { useConfig } from "@/app/store/useConfig";
 import { useUserAgent } from "@/app/context/user-agent";
+import { ReportDataType, reportTradeData } from "@/app/utils/report";
 
 type Token = {
   tokenName: string;
@@ -654,6 +655,8 @@ export default function BuySellPump({
                         Big(volume).toFixed(SOL.tokenDecimals),
                         "pump"
                       );
+
+                      reportTradeData(ReportDataType.SWAP, hash);
 
                       const modalHandler = Modal.show({
                         content: (
---
+++ app/utils/report.ts
@@ -0,0 +1,30 @@
+import { httpAuthPost } from "@/app/utils";
+
+export async function reportTradeData(type: number, txHash: string, uuid: string = '') {
+  try {
+    const data = {
+      t: type, // 2 for swap, 3 for flip
+      v: JSON.stringify({
+        uuid,
+        tx_hash: txHash
+      })
+    };
+
+    const response = await httpAuthPost('/report/data', data);
+
+    if (!response.ok) {
+      throw new Error('Report data failed');
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error reporting trade data:', error);
+    return null;
+  }
+}
+
+
+export const ReportDataType = {
+  SWAP: 2,
+  FLIP: 3
+} as const;
