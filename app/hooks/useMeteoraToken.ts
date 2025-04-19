b8f234f8c56c97327e8f06601b0bad7ef4635976
ben <benjm240@gmail.com>
2025-04-19 22:08:10 +0800

    test build

--- app/hooks/useMeteoraToken.ts
+++ app/hooks/useMeteoraToken.ts
@@ -1,26 +1,26 @@
-import { PublicKey } from '@solana/web3.js';
-import type { Project } from '../type';
-import { useAccount } from "./useAccount";
-import { VirtualCurveClient } from "@meteora-ag/virtual-curve-sdk";
-import { useConnection } from '@solana/wallet-adapter-react';
-import { useEffect } from 'react';
+// import { PublicKey } from '@solana/web3.js';
+// import type { Project } from '../type';
+// import { useAccount } from "./useAccount";
+// import { VirtualCurveClient } from "@meteora-ag/virtual-curve-sdk";
+// import { useConnection } from '@solana/wallet-adapter-react';
+// import { useEffect } from 'react';
 
 
 
-export const useMeteoraToken = () => {
-  const { publicKey } = useAccount();
-  const { connection } = useConnection()
+// export const useMeteoraToken = () => {
+//   const { publicKey } = useAccount();
+//   const { connection } = useConnection()
 
-  useEffect(() => {
-    const client = new VirtualCurveClient(connection)
+//   useEffect(() => {
+//     const client = new VirtualCurveClient(connection)
 
-    console.log('client', client)
+//     console.log('client', client)
 
-  }, [])
+//   }, [])
 
  
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
-  return {
+//   return {
 
-  };
-};
+//   };
+// };
