88eb258a10e9eb03c420ea7317aff5ceecaa304c
ben <benjm240@gmail.com>
2025-04-24 22:17:12 +0800

    fix bugs

--- app/sections/mining/laptop/index.tsx
+++ app/sections/mining/laptop/index.tsx
@@ -18,7 +18,8 @@ export default function Laptop({
   userInfo,
   rate,
   rateLoading,
-  onQuery
+  onQuery,
+  claimRaydiumFee
 }: any) {
   const configStore: any = useConfig((store: any) => store.config);
 
@@ -40,7 +41,7 @@ export default function Laptop({
       >
         <div className={styles.Left}>
           <Panel isTape className={styles.TotalPanel}>
-            <TotalPanel info={info} userInfo={userInfo} />
+            <TotalPanel info={info} userInfo={userInfo} claimRaydiumFee={claimRaydiumFee} />
           </Panel>
           <Panel isTape={false} className={styles.PanelWrapper}>
             <div className={styles.PanelTitle}>Earn</div>
