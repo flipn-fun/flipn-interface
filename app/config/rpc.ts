277153dbbea12238ea9b3ae98b3d1405c4ad1fbc
ben <benjm240@gmail.com>
2025-07-03 21:21:44 +0800

    update rpc

--- app/config/rpc.ts
+++ app/config/rpc.ts
@@ -6,15 +6,15 @@ export const RPC_COLORS = {
 
 export const RPCS = [
   {
+    name: "Chainstack",
+    url: "https://solana-mainnet.core.chainstack.com/e7c21746149c2d33eb471f295dd98707"
+  },
+  {
     name: "Helius",
-    url: "https://mainnet.helius-rpc.com/?api-key=28fc7f18-acf0-48a1-9e06-bd1b6cba1170"
+    url: "https://mainnet.helius-rpc.com/?api-key=7645ce3e-4ffc-4ff1-b786-1205f2e07c0b"
   },
   {
     name: "Flipn",
     url: "https://flipn.minirpc.top"
   },
-  {
-    name: "Chainstack",
-    url: "https://solana-mainnet.core.chainstack.com/e7c21746149c2d33eb471f295dd98707"
-  }
 ];
